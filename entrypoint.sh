26660ed041b8826c8436fb5773dd238d8b23c709
Andrew Cole <andrew.cole@illallangi.com>
2020-07-11 11:05:34 +1000

    Added check for existance of certificate before self signed generation

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,18 +2,23 @@
 
 confd -onetime -backend env -log-level debug || exit 1
 
-openssl \
-  req \
-    -x509 \
-    -sha256 \
-    -newkey rsa:4096 \
-    -keyout key.pem \
-    -out cert.pem \
-    -days 365 \
-    -nodes \
-    -subj "/C=AU/ST=Victoria/L=Melbourne/O=Illallangi Enterprises/OU=OIDC Proxy/CN=${FQDN}" \
-    -keyout /tls/tls.key \
-    -out    /tls/tls.crt \
-  || exit 1
+if [ -f /tls/tls.crt]; then
+  echo "Using supplied certificate"
+else
+  openssl \
+    req \
+      -x509 \
+      -sha256 \
+      -newkey rsa:4096 \
+      -keyout key.pem \
+      -out cert.pem \
+      -days 365 \
+      -nodes \
+      -subj "/C=AU/ST=Victoria/L=Melbourne/O=Illallangi Enterprises/OU=OIDC Proxy/CN=${FQDN}" \
+      -keyout /tls/tls.key \
+      -out    /tls/tls.crt \
+    || exit 1
+  echo "Generated self signed certificate"
+fi
 
 exec ${*:-/usr/local/openresty/nginx/sbin/nginx -g "daemon off;"} || exit 1
